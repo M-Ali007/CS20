3d54f6408558bd732f01203a95ff1ee87a2e1de6
M-Ali007 <mustafa.murta08@gmail.com>
2026-03-27 10:07:05 -0600

    Added Comments to TI50

--- Ali_Mustafa - TI-150.py
+++ Ali_Mustafa - TI-150.py
@@ -1,33 +1,33 @@
 # Mustafa Ali
 # Assignment 3 - TI-150
 # Block 5
-# March 24th 2026
+# March 27th 2026
 
 # This program is my own work - MA
 
+#imports
 import tkinter as tk
 from tkinter import ttk
 
-TBT = ""
-
 def add(value):
-    my_text.insert(tk.END, value)
+    my_text.insert(tk.END, value) #adds value to the textbox
 
 def clear():
-    my_text.delete("1.0", tk.END)
+    my_text.delete("1.0", tk.END) # clears the values in the text box
 
 def calculate():
-    expression = my_text.get("1.0", tk.END)
+    expression = my_text.get("1.0", tk.END) # gets the contents of the textbox
     try:
-        result = eval(expression)
+        result = eval(expression). #Python Function that takes a string and returns answer
     except SyntaxError:
-        result = "ERROR - PROPER EXPRESSION ONLY PLEASE"
+        result = "ERROR - PROPER EXPRESSION ONLY PLEASE". # Error checking for bad input
     except ZeroDivisionError:
-        result = "Undefined - You cannot divide by zero"
+        result = "Undefined - You cannot divide by zero" # Error chekcing for zero divisions
 
     my_text.delete("1.0", tk.END)
     my_text.insert(tk.END, result)
 
+# Button mappings to loop through to creating all the buttons 
 buttons = {
     "7":  {"column": 1, "row": 3, "command": add},
     "8":  {"column": 2, "row": 3, "command": add},
@@ -46,18 +46,22 @@ buttons = {
 }
 
 
-root = tk.Tk()
-root.title("MustaCalc")
-frame = ttk.Frame(root, padding=10)
-frame.grid()
-my_text = tk.Text(frame, wrap='word', height=2, width=20)
-my_text.grid(column=1, row=2, columnspan=4, sticky='ew')
+root = tk.Tk(). # creates tkinter instance
+root.title("MustaCalc"). # window name
+frame = tk.Frame(root, padding=10) # initializes tkinter frame
+frame.grid() # initializes grid layout
+my_text = tk.Text(frame, wrap='word', height=2, width=20) # main text box
+my_text.grid(column=1, row=2, columnspan=4, sticky='ew') # creates grid layout
+
+# for loop loops through the dictionary to create the buttons
 for label, pos in buttons.items():
     tk.Button(frame, text=label, width=4, height=2, command=lambda v=label: add(v)).grid(
         column=pos["column"], row=pos["row"]
     )
+
+# Clear, equals to and close buttons seperate because they have different functions
 tk.Button(frame, text="C", width=4, height=2, command=clear).grid(column=1, row=6)
 tk.Button(frame, text="=", width=4, height=2, command=calculate).grid(column=3, row=6)
 tk.Button(frame, text="x", width=1, height=1,command=root.destroy).grid(column=4, row=1)
-root.mainloop()
+root.mainloop() # starts tkinter render loop
 
